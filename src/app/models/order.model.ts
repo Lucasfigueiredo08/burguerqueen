be12ce719c8273e7610080ae2fa72626350e3223
Lucas Figueiredo <lukas.liberato14@gmail.com>
2025-02-21 12:25:59 -0300

    199 - Calcula o total do nosso pedido

--- src/app/models/order.model.ts
+++ src/app/models/order.model.ts
@@ -1,11 +1,13 @@
-import { Signal, signal, WritableSignal } from "@angular/core";
+import { inject, Signal, signal, WritableSignal } from "@angular/core";
 import { IQuantityProduct } from "./quantity-product.model";
 import { IProduct } from "./product.model";
+import { CalculateTotalPricePipe } from "../pipes/calculate-total-price.pipe";
 
 
 export class Order {
 
     private _productsSignal: WritableSignal<IQuantityProduct[]> = signal<IQuantityProduct[]>([]);
+    private calculateTotalPricePipe = inject(CalculateTotalPricePipe);
 
     public get productsSignal(): Signal<IQuantityProduct[]> {
         return this._productsSignal.asReadonly();
@@ -29,6 +31,11 @@ export class Order {
 
     private searchProduct(product: IProduct) {
         return this._productsSignal().find((productQuantity: IQuantityProduct) => JSON.stringify(product) === JSON.stringify(productQuantity.product));
+    }
+
+    private totalOrder() {
+        return this._productsSignal().reduce(
+            (acumulador: number, value: IQuantityProduct) => this.calculateTotalPricePipe.transform(value.product, value.quantity) + acumulador, 0)
     }
 
 }
