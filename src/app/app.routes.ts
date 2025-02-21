2a68a25a1ca32cfa620b10969b97a6d9383ff0e1
Lucas Figueiredo <lukas.liberato14@gmail.com>
2025-02-21 17:21:21 -0300

    guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { CategoriesComponent } from './components/categories/categories.componen
 import { ProductsComponent } from './components/products/products.component';
 import { ProductComponent } from './components/product/product.component';
 import { PayOrderComponent } from './components/pay-order/pay-order.component';
+import { payOrderGuard } from './guard/pay-order.guard';
 
 export const routes: Routes = [
     { path: 'categories', component: CategoriesComponent },
     { path: 'products/:categoryId', component: ProductsComponent },
     { path: 'product/:id', component: ProductComponent },
-    { path: 'pay-order', component: PayOrderComponent },
+    { path: 'pay-order', component: PayOrderComponent, canActivate: [payOrderGuard] },
     { path: '**', redirectTo: 'categories' }
 ];
