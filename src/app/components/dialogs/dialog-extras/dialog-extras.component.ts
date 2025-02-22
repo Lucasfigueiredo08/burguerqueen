b811a555a18c0ec9a2da8f02ff287b9763904585
Lucas Figueiredo <lukas.liberato14@gmail.com>
2025-02-22 16:42:39 -0300

    Conteúdo do nosso dialogo de extras

--- src/app/components/dialogs/dialog-extras/dialog-extras.component.ts
+++ src/app/components/dialogs/dialog-extras/dialog-extras.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent } from '@angular/material/dialog';
+import { IProductExtraBlock } from '../../../models/product.model';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatButton } from '@angular/material/button';
+import { JsonPipe } from '@angular/common';
+import { ExtraSelectedPipe } from '../../../pipes/extra-selected.pipe';
 
 @Component({
   selector: 'app-dialog-extras',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule,
+    MatDialogContent,
+    MatDialogActions,
+    MatButton,
+    MatDialogClose, 
+    ExtraSelectedPipe
+  ],
   templateUrl: './dialog-extras.component.html',
   styleUrl: './dialog-extras.component.scss'
 })
 export class DialogExtrasComponent {
 
+
+  private dataDialog = inject(MAT_DIALOG_DATA);
+  public extraBlocks: IProductExtraBlock[] = this.dataDialog['extraBlocks'];
 }
