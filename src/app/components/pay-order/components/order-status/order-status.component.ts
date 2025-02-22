ace621bc0ea5e0a26cb0b0e4ea63850f3ed1f4d8
Lucas Figueiredo <lukas.liberato14@gmail.com>
2025-02-22 15:42:35 -0300

    209 - Mostrando produtos com MatTable

--- src/app/components/pay-order/components/order-status/order-status.component.ts
+++ src/app/components/pay-order/components/order-status/order-status.component.ts
@@ -1,14 +1,25 @@
+import { CalculateTotalPricePipe } from './../../../../pipes/calculate-total-price.pipe';
 import { Component, inject } from '@angular/core';
 import { UserOrderService } from '../../../../services/user-order.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { JsonPipe } from '@angular/common';
+import { MatCellDef, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef, MatTable } from '@angular/material/table'
 
 @Component({
   selector: 'app-order-status',
   standalone: true,
   imports: [
     TranslateModule,
-    JsonPipe
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatHeaderCellDef,
+    MatHeaderRow,
+    MatHeaderRowDef,
+    MatRow,
+    MatRowDef,
+    MatCellDef,
+    CalculateTotalPricePipe
   ],
   templateUrl: './order-status.component.html',
   styleUrl: './order-status.component.scss'
@@ -19,6 +30,7 @@ export class OrderStatusComponent {
 
   public productsSignal = this.userOrderService.productsSignals;
   public totalOrderSignal = this.userOrderService.totalOrderSignal;
+  public displayedColumns: string[] = ['name', 'price', 'quantity', 'total'];
 
 
 }
