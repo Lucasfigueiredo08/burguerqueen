2a4937be4d123da3eb8c39c984ca2e71608907d9
Lucas Figueiredo <lukas.liberato14@gmail.com>
2025-02-22 11:34:48 -0300

    207 - Passos para pagar com MatStepper

--- src/app/components/pay-order/pay-order.component.ts
+++ src/app/components/pay-order/pay-order.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatStepper, MatStep, MatStepLabel, MatStepperNext, MatStepperPrevious } from '@angular/material/stepper';
+import { MatButton } from '@angular/material/button';
+import { OrderStatusComponent } from '../order-status/order-status.component';
 
 @Component({
   selector: 'app-pay-order',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule, 
+    MatStep, 
+    MatStepper,
+    MatStepLabel,
+    MatStepperNext,
+    MatStepperPrevious,
+    MatButton,
+    OrderStatusComponent
+  ],
   templateUrl: './pay-order.component.html',
   styleUrl: './pay-order.component.scss'
 })
