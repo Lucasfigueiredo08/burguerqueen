f730abffc756fa5dbed2837a42a4114655589285
Lucas Figueiredo <lukas.liberato14@gmail.com>
2025-02-23 14:11:45 -0300

    216 - Preparando nosso serviço de stripe, criado conta no stripe e etc.

--- src/app/components/pay-order/pay-order.component.ts
+++ src/app/components/pay-order/pay-order.component.ts
@@ -1,8 +1,16 @@
+import { StripeElement } from './../../../../node_modules/@stripe/stripe-js/dist/stripe-js/elements-group.d';
+import { StripeService } from './../../services/stripe.service';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatStepper, MatStep, MatStepLabel, MatStepperNext, MatStepperPrevious } from '@angular/material/stepper';
 import { MatButton } from '@angular/material/button';
 import { OrderStatusComponent } from './components/order-status/order-status.component';
+import { environment } from '../../../environments/environment.development';
+import { injectStripe, StripeElementsDirective, StripePaymentElementComponent } from 'ngx-stripe';
+import {
+  StripeElementsOptions,
+  StripePaymentElementOptions
+} from '@stripe/stripe-js'
 
 @Component({
   selector: 'app-pay-order',
@@ -15,11 +23,37 @@ import { OrderStatusComponent } from './components/order-status/order-status.com
     MatStepperNext,
     MatStepperPrevious,
     MatButton,
-    OrderStatusComponent
+    OrderStatusComponent,
+    StripeElementsDirective,
+    StripePaymentElementComponent,
   ],
   templateUrl: './pay-order.component.html',
   styleUrl: './pay-order.component.scss'
 })
 export class PayOrderComponent {
+
+  public stripe = injectStripe(environment.stripe.publishKey);
+
+  elementsOptions: StripeElementsOptions = {
+    locale: 'en',
+    appearance: {
+      theme: 'flat'
+    }
+  };
+
+  paymentElementOptions: StripePaymentElementOptions = {
+    layout: {
+      type: 'tabs',
+      defaultCollapsed: false,
+      radios: false,
+      spacedAccordionItems: false
+    }
+  };
+
+  payOrder() {
+
+  }
+
+
 
 }
--- src/app/services/stripe.service.ts
+++ src/app/services/stripe.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { ICreatePaymentIntent } from '../models/create-payment-intent.model';
+import { first } from 'rxjs';
+import { IPayment } from '../models/payment.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StripeService {
 
-  constructor() { }
+    private URL_BASE = `${environment.urlServer}/v1/stripe`; 
+    private http = inject(HttpClient);
+
+  createPaymentSheet(paymentIntent : ICreatePaymentIntent) {
+
+    const headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${paymentIntent.secretKey}`
+    })
+
+    return this.http.post<IPayment>(`${this.URL_BASE}/intent`, paymentIntent, { headers }).pipe(first());
+  }
 }
