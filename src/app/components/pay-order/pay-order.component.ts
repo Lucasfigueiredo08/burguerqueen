91617587bcdb051ab03d6f5d5044f4788950ffa3
Lucas Figueiredo <lukas.liberato14@gmail.com>
2025-02-23 22:00:03 -0300

    217 - Criar um payment intent

--- src/app/components/pay-order/pay-order.component.ts
+++ src/app/components/pay-order/pay-order.component.ts
@@ -1,6 +1,5 @@
-import { StripeElement } from './../../../../node_modules/@stripe/stripe-js/dist/stripe-js/elements-group.d';
 import { StripeService } from './../../services/stripe.service';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatStepper, MatStep, MatStepLabel, MatStepperNext, MatStepperPrevious } from '@angular/material/stepper';
 import { MatButton } from '@angular/material/button';
@@ -11,13 +10,17 @@ import {
   StripeElementsOptions,
   StripePaymentElementOptions
 } from '@stripe/stripe-js'
+import { UserOrderService } from '../../services/user-order.service';
+import { ICreatePaymentIntent } from '../../models/create-payment-intent.model';
+import { IPayment } from '../../models/payment.model';
+import { StepperSelectionEvent } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-pay-order',
   standalone: true,
   imports: [
-    TranslateModule, 
-    MatStep, 
+    TranslateModule,
+    MatStep,
     MatStepper,
     MatStepLabel,
     MatStepperNext,
@@ -31,6 +34,9 @@ import {
   styleUrl: './pay-order.component.scss'
 })
 export class PayOrderComponent {
+
+  private stripeService = inject(StripeService);
+  private userOrderService = inject(UserOrderService);
 
   public stripe = injectStripe(environment.stripe.publishKey);
 
@@ -49,6 +55,30 @@ export class PayOrderComponent {
       spacedAccordionItems: false
     }
   };
+
+  public totalOrderSignal = this.userOrderService.totalOrderSignal;
+  public lastTotal: number = 0;
+
+  createPaymentIntent(event: StepperSelectionEvent) {
+
+    if (event.selectedIndex == 1 && ( !this.elementsOptions.clientSecret ||  this.lastTotal != this.totalOrderSignal())) {
+
+      const amount = this.totalOrderSignal() * 100;
+
+      const paymentIntent: ICreatePaymentIntent = {
+        secretKey: environment.stripe.secretKey,
+        amount: +amount.toFixed(0),
+        currency: 'EUR',
+        customer_id: environment.stripe.customer_id
+      }
+
+      this.stripeService.createPaymentSheet(paymentIntent).subscribe({
+        next: (paymentIntent: IPayment) => {
+          this.elementsOptions.clientSecret = paymentIntent.paymentIntentClientSecret;
+        }
+      })
+    }
+  }
 
   payOrder() {
 
